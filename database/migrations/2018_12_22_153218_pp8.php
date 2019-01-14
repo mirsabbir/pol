c2ae0edd63801caf509c4d809235f1937cd37a8d
sabbir <mirsabbiralam@gmail.com>
2019-01-15 01:52:44 +0600

    op

--- database/migrations/2018_12_22_153218_pp8.php
+++ database/migrations/2018_12_22_153218_pp8.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Pp extends Migration
+class Pp8 extends Migration
 {
     /**
      * Run the migrations.
